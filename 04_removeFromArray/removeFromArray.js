0fb041e9cac0ea5def03f56174ed76029c6c0e9b
Odinsondev <122601475+Odinsondev@users.noreply.github.com>
2023-06-25 00:08:39 -0700

    Complete exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...values) {
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] === undefined) {
+      return array
+    } else if ( !(array.includes(values[i])) ) {
+      continue
+    } else {
+    let index = array.indexOf(values[i])
+    let removedValue = array.splice(index, 1)
+    }
+  }
+  return array
 };
 
 // Do not edit below this line
