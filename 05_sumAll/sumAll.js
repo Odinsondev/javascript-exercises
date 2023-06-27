5cec4e9be0e54b9f458173bc0a4c02ed437ba6a9
Odinsondev <122601475+Odinsondev@users.noreply.github.com>
2023-06-27 01:13:31 -0700

    Solve test1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  let result = 0
+  for (let i = num1; i <= num2; i++) {
+    result = result + i
+  }
+  return result
 };
 
 // Do not edit below this line
