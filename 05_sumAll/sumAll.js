d6dccad5d677f3dc07c7ae7a24cc52f3496aed60
Odinsondev <122601475+Odinsondev@users.noreply.github.com>
2023-06-28 01:02:30 -0700

    Solve exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,20 @@
 const sumAll = function(num1, num2) {
   let result = 0
-  for (let i = num1; i <= num2; i++) {
-    result = result + i
-  }
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR"
+  } else if ( typeof num1 !== "number" || typeof num2 !== "number") {
+    return "ERROR"
+  } else if (num2 > num1) {
+    for (let i = num1; i <= num2; i++) {
+      result = result + i
+    }
+    return result
+  } else {
+    for (let i = num2; i <= num1; i++) {
+      result = result + i
+    }
   return result
+  }
 };
 
 // Do not edit below this line
