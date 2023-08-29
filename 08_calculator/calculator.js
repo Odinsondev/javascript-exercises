762e35d12ba4563520a4157cc0858f09466e91eb
Odinsondev <122601475+Odinsondev@users.noreply.github.com>
2023-08-29 04:32:51 -0700

    Solve javascript exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
+const add = function(num1, num2) {   // only works for 2 numbers
+	return num1 + num2;
+};
+
+const subtract = function(num1, num2) {  // only works for 2 numbers
+	return num1 - num2;
 	
 };
 
-const subtract = function() {
-	
+const sum = function(array) {  // only works for arrays
+  let total = 0;
+  for (let i = 0; i < array.length; i++) {
+    total = total + array[i];
+  }
+	return total;
 };
 
-const sum = function() {
-	
+const multiply = function(...values) {
+  let answer = values[0];
+  for (let i = 1; i <values.length; i++) {
+    answer = answer * values[i];
+  }
+  return answer;
 };
 
-const multiply = function() {
-
+const power = function(num1, powerOf) {
+  let answer = num1;
+	for (let i = 1; i < powerOf; i++ ) {
+    answer = answer * num1;
+  }
+  return answer;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num1) {
+  let answer = 1;
+  if (num1 === 0) {
+    return answer;
+  } else {
+    for (let i = 1; i <= num1; i++) {
+      answer = answer * i;
+    }
+    return answer;
+  }
 };
 
 // Do not edit below this line
