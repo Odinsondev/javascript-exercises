d685ce034b979a2975e14983bc3c26923cafa2c9
Odinsondev <122601475+Odinsondev@users.noreply.github.com>
2023-08-30 01:16:01 -0700

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if (num <= 0) {
+    return "OOPS";
+  } else {
+    let array = [0,1,1];
+    for (let i = 3; i <= num; i++) {
+      array[i] = array[i - 2] + array[i - 1];
+    }
+    return array[num];
+  }
 };
 
 // Do not edit below this line
