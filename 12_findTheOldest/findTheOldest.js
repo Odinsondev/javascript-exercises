958eff22d3ba534c3a3389f1eb037b066427a420
Odinsondev <122601475+Odinsondev@users.noreply.github.com>
2023-09-01 21:21:31 -0700

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArray) {
+  for (let i = 0; i < peopleArray.length; i++) {
+    if (peopleArray[i].yearOfDeath === undefined) {
+      peopleArray[i].yearOfDeath = (new Date()).getFullYear();
+    }
+  }
+  const orderedPeople = peopleArray.sort(function(a, b) {
+    if ((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)) {      
+      return -1;
+    } else {
+      return +1;
+    } 
+  });
+return orderedPeople[0]
 };
 
 // Do not edit below this line
